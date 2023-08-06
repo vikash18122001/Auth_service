cff295f55094c824fe71915ba4402cec093d98ed
vikash maurya <vikashghazipur2001@gmail.com>
2023-08-06 21:50:03 +0530

    added roles and user_roles through tables

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ const express=require('express');
 const app=express();
 const apiRoutes=require('./routes/index');
 const bodyParser=require('body-parser');
+const db=require('./models/index')
 
 
 const {PORT}=require('./config/serverConfig')
  startServer=async ()=>{
+    
   
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',apiRoutes);
     app.listen(PORT,()=>{
         console.log(`server is running on port:${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true});
+        }
     })
 }
 
